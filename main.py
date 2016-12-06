a83bf94c62b9121b6f23715b729f2bda1c972f66
kcrong <kcrong@kim82536.pe.kr>
2016-12-06 16:36:14 +0900

    파이썬삼지원  커맨드에서 파일IO로 바꿈

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
+import sys
+
+print("Domain Ex) .naver.com")
+
+if sys.version_info.major == 3:
+    domain = input("Domain: ")
+else:
+    domain = raw_input("Domain: ")
+
+
 cookie_command = ""
 
-raw_cookies = input("Cookie: ")
+try:
+    f = open("input_cookie.txt", "r")
+except IOError:
+    print("where is 'input_cookie.txt' ?")
+else:
+    raw_cookies = f.read()
 
-for cookie in raw_cookies.split(';'):
-    eq_index = cookie.find('=')
+    for cookie in raw_cookies.split(';'):
+        eq_index = cookie.find('=')
 
-    cookie_command += 'document.cookie="%s=%s";' % (cookie[:eq_index], cookie[eq_index + 1:])
+        cookie_command += 'document.cookie="%s=%s";domain="%s";path="/"' % (cookie[:eq_index], cookie[eq_index + 1:], domain)
 
-print("------------------------------------")
-print(cookie_command[:-1])
+    print("------------------------------------")
+    print(cookie_command[:-1])
+    with open("output_cookie.txt", "w") as f:
+        f.write(cookie_command[:-1])
+    print("\n\n")
+    print("By Kcrong :)")
